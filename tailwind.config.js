9367f046abb3d4d2911f9b2e61f55601631b56db
Bodoi <109707645+kieuduc123@users.noreply.github.com>
2023-08-15 17:14:58 +0700

    up load

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       boxShadow :{
-        '3xl' : '[7px 7px 0px 0px rgba(0, 0, 0, 0.3)]',
+        '3xl' : '[-7px 7px 0px 0px rgba(0, 0, 0, 0.3)]',
       }
     },
     fontFamily :{
